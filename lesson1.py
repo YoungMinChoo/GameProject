ab61b18c811093a2c38077d942a6c30956f059dd
Faye-Choo <hjfayechoo@gmail.com>
2024-05-12 18:26:46 -0400

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+# Count the number of names in the list that have the letter M in it (Upper case only)
+
+names = ['Beth', 'Margaret', 'Bob', 'Faye', 'Kristen', 'Luqman', 'Mom', 'Dad', 'Min', 'Johnny']
+
+c = names.count('M')
+print(c)
